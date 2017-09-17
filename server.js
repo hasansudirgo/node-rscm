dc17691275d5bc3404b158d15ba9aeeea7d8ec0f
hsurya <hasan_surya@yahoo.com>
2017-09-18 05:11:45 +0700

    coba

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var io = require('socket.io')(http);
 //app.options('/', cors());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
