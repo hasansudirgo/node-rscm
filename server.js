34e2a85d6ffe8e7e28bcd1c369c20039f8c0ebf1
hsurya <hasan_surya@yahoo.com>
2017-09-17 19:31:22 +0700

    coba

--- server.js
+++ server.js
@@ -3,18 +3,19 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var enableCORS = function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-   Length, X-Requested-With');
-	if ('OPTIONS' == req.method) {
-  	res.send(200);
-	}else {
-  	next();
-	}
-};
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.use(enableCORS);
+app.get('/', function(req, res, next) {
+  // Handle the get for this route
+});
+
+app.post('/', function(req, res, next) {
+ // Handle the post for this route
+});
 
 app.use(express.static( __dirname + '/' ));
 app.get('/', function(req, res){
