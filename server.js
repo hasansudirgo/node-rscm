d2ce141c9f1391db4dd8a5f337f48b2524abae25
hsurya <hasan_surya@yahoo.com>
2017-09-17 19:04:46 +0700

    coba

--- server.js
+++ server.js
@@ -3,18 +3,12 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var enableCORS = function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-   Length, X-Requested-With');
-	if ('OPTIONS' == req.method) {
-  	res.send(200);
-	}else {
-  	next();
-	}
-};
+const cors = require('cors');
+const express = require('express');
+let app = express();
+app.use(cors());
+app.options('*', cors());
 
-app.use(enableCORS);
 app.use(express.static( __dirname + '/' ));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
